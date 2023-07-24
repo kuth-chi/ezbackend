68242e2b987a862820f726663c9ff890790030ff
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-07-24 23:56:59 +0700

    Configure Vercel Data

--- main/main/settings.py
+++ main/main/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': '5432',  # Default PostgreSQL port is 5432
     }
 }
 
