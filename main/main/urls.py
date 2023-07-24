5df32c912c3f3dc36d6e871d33aaf4fcbfaa775c
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-07-25 01:47:11 +0700

    Update file

--- main/main/urls.py
+++ main/main/urls.py
@@ -19,6 +19,6 @@ if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += [
         re_path(r'^rosetta', include('rosetta.urls'))
     ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
