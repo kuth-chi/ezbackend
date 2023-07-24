cc8805d943c9aca3b474804d33b5b6426c189835
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-07-24 22:58:23 +0700

    Update staticfile root in settings

--- main/main/settings.py
+++ main/main/settings.py
@@ -140,6 +140,10 @@ LOCALE_PATHS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Production directories
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
 # MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
