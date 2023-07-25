97238d0b4e28d74a4412f827a9cece32c10ece14
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-07-25 10:56:20 +0700

    Update vercel json

--- main/build_files.sh
+++ main/build_files.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 echo "BUILD START"
  python3.11 -m pip install -r requirements.txt
+ python3.11 -m pip install --upgrade pip --noinput
+
+echo "Migrating Databases..."
+ python3.11 manage.py makemigrations --noinput
+ python3.11 manage.py migrate --noinput
+
+ echo "Collecting Staticfiles..."
  python3.11 manage.py collectstatic --noinput --clear
-echo "BUILD END"
